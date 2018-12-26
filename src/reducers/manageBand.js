4c7eb6edf497a48bf9610ec4011eaef35796bf83
ihollander <ianholla@gmail.com>
2018-12-26 18:09:24 -0500

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -3,8 +3,16 @@ export default function manageBand(state = {
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
+      const maxId = Math.max(...state.bands.map(b => b.id))
+      const id = maxId > 0 ? maxId + 1 : 1
+      const band = {
+        name: action.name,
+        id: id
+      }
+      return { ...state, bands: [...state.bands, band] }
 
-      return { ...state, bands: [...state.bands, action.name] }
+    case 'DELETE_BAND':
+      return { ...state, bands: state.bands.filter(band => band.id !== action.id) }
 
     default:
       return state;
